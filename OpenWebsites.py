4ffbd891797c091c7c135e884c4870ca6a48e561
Adithya Dixit <adithyadixit2522@gmail.com>
2024-02-10 11:54:18 +0400

    Added lines

--- OpenWebsites.py
+++ OpenWebsites.py
@@ -1,3 +1,18 @@
-import webbrowser as wb
+import webbrowser
 
-wb.open("https://youtube.com")
+# List of social media URLs
+socialMedia = ["instagram", "twitter", "reddit", "youtube"]
+
+# Open a new text file in write mode ('w')
+with open("Social media address.txt", 'w') as smAddress:
+    # The list is joined to be a string as the write only takes strings as a whole
+    socialMediaString = "\n".join(socialMedia)
+    # Write the string to the file
+    smAddress.write(socialMediaString)
+
+# Open each URL in a web browser
+for name in socialMedia:
+    # Construct the complete URL
+    url = "https://www." + name + ".com"
+    # Open the URL in the default web browser
+    webbrowser.open(url)
